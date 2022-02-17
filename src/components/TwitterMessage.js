9edd07fe1a42012f7c0fd5133b03380946871539
Andrew Eames <ajerxs@gmail.com>
2022-02-16 19:51:26 -0500

    commit tree 8c4d25f140aed4516b06e846205ca3516a609d76

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
+  }
+
+  saveMessage = (event) => {
+    this.setState({
+      message: event.target.value,
+    })
+  }
+  
+  charsRemaining = () => {
+    return this.props.maxChars - this.state.message.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={(event) => this.saveMessage(event)} />
+        <div>{this.charsRemaining()}</div>
       </div>
     );
   }
